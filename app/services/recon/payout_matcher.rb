3a0580e5ad5814c1f5fff888d1dcbb657ed28af6
Miguel Ricardo Gomez <miguelgomez66@gmail.com>
2025-09-20 21:28:57 -0300

    Add payout transaction modal and matching logic (#27)

--- app/services/recon/payout_matcher.rb
+++ app/services/recon/payout_matcher.rb
@@ -17,21 +17,103 @@ module Recon
       scope_matches.delete_all
 
       adyen_payouts.each do |p|
-        match = @bank_lines.find { |b| b[:currency] == p[:currency] && b[:amount_cents].to_i == p[:amount_cents].to_i && (b[:date] - p[:date]).abs <= 2 }
-        status, details = if match
-                            [:matched, { bank_ref: match[:ref], bank_amount_cents: match[:amount_cents] }]
-                          else
-                            [:unmatched, {}]
-                          end
         payout_currency = p[:currency] || @currency_filter
+        capture_result = capture_transactions_until(payout_date: p[:date], currency: payout_currency)
+        transactions = capture_result[:transactions]
+
+        transactions_total = transactions.sum { |tx| tx[:amount_cents].to_i }
+        payout_amount = p[:amount_cents].to_i.abs
+
+        match = @bank_lines.find do |b|
+          b[:currency] == payout_currency &&
+            b[:amount_cents].to_i == p[:amount_cents].to_i &&
+            (b[:date] - p[:date]).abs <= 2
+        end
+
+        details = {}
+        details[:bank_ref] = match[:ref] if match
+        details[:transactions] = transactions
+        details[:transactions_total_cents] = transactions_total
+        details[:transactions_period_start] = capture_result[:period_start]&.to_s
+
+        status = if payout_amount == transactions_total
+                   :matched
+                 elsif transactions_total.zero?
+                   :unmatched
+                 else
+                   :partial
+                 end
 
         PayoutMatch.create!(
-          account_scope: @scope, payout_date: p[:date], currency: payout_currency,
-          adyen_payout_id: p[:id], adyen_amount_cents: p[:amount_cents],
-          bank_ref: details[:bank_ref], bank_amount_cents: details[:bank_amount_cents],
-          status:, details: details.except(:bank_amount_cents)
+          account_scope: @scope,
+          payout_date: p[:date],
+          currency: payout_currency,
+          adyen_payout_id: p[:id],
+          adyen_amount_cents: p[:amount_cents],
+          bank_ref: match&.[](:ref),
+          bank_amount_cents: match&.[](:amount_cents),
+          status:,
+          details: details
         )
       end
+    end
+
+    private
+
+    def capture_transactions_until(payout_date:, currency:)
+      stmt_model = Sources::Config::StatementLine
+      return { transactions: [], period_start: nil } unless stmt_model
+
+      relation = stmt_model.joins("INNER JOIN report_files rf ON rf.id = statement_lines.#{Sources::Config::SL_FILE_ID}")
+      relation = if @scope.nil?
+                   relation.where("COALESCE(rf.#{Sources::Config::RF_SCOPE}, '') = ''")
+                 else
+                   relation.where("rf.#{Sources::Config::RF_SCOPE} = ?", @scope)
+                 end
+
+      relation = relation.where(<<~SQL, currency, currency)
+        (statement_lines.#{Sources::Config::SL_CURRENCY} = ?
+          OR (statement_lines.#{Sources::Config::SL_CURRENCY} IS NULL OR statement_lines.#{Sources::Config::SL_CURRENCY} = '')
+             AND rf.#{Sources::Config::RF_CURRENCY} = ?)
+      SQL
+
+      relation = relation.where("LOWER(statement_lines.#{Sources::Config::SL_CATEGORY}) IN (?)", %w[payment platformpayment])
+                         .where("LOWER(statement_lines.#{Sources::Config::SL_TYPE}) = 'capture'")
+
+      previous_date = previous_payout_date(payout_date:, currency:)
+
+      relation = relation.where("statement_lines.#{Sources::Config::SL_BOOK_DATE} <= ?", payout_date)
+      relation = relation.where("statement_lines.#{Sources::Config::SL_BOOK_DATE} > ?", previous_date) if previous_date
+
+      rows = relation.order("statement_lines.#{Sources::Config::SL_BOOK_DATE} ASC, statement_lines.line_no ASC")
+                      .pluck(Sources::Config::SL_BOOK_DATE, "statement_lines.reference", Sources::Config::SL_AMOUNT)
+
+      transactions = rows.map do |date, reference, amount|
+        {
+          date: date&.to_s,
+          reference: reference,
+          amount_cents: amount.to_i
+        }
+      end
+
+      { transactions:, period_start: previous_date }
+    end
+
+    def previous_payout_date(payout_date:, currency:)
+      payout_model = Sources::Config::PayoutModel
+      return nil unless payout_model
+
+      relation = payout_model.left_joins(:source_report_file)
+      relation = if @scope.nil?
+                   relation.where("COALESCE(report_files.#{Sources::Config::RF_SCOPE}, '') = ''")
+                 else
+                   relation.where("report_files.#{Sources::Config::RF_SCOPE} = ?", @scope)
+                 end
+
+      relation = relation.where("COALESCE(payouts.currency, report_files.#{Sources::Config::RF_CURRENCY}) = ?", currency)
+      relation = relation.where("payouts.booked_on < ?", payout_date)
+
+      relation.order(booked_on: :desc).limit(1).pick(:booked_on)
     end
   end
 end
